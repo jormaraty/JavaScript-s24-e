51acb0a9cd917699496abddedc77acb183aa6ce1
jormaraty <jorma.raty@metropolia.fi>
2024-11-17 18:01:13 +0200

    toinen versio

--- mod03/module3-starters-main/t2/2.js
+++ mod03/module3-starters-main/t2/2.js
@@ -3,16 +3,19 @@
 // etsitään <ul> määritys html sivulta
 const ulElem = document.querySelector('#target');
 
-// tehdään eka li-elementit
+// tehdään eka li-elementti
 let li1Elem = document.createElement('li');
 // tehtään tekstiElementti
 let text1Elem = document.createTextNode('First item');
 // lisätään li-elementin sisään eli lapseksi äsken luotu teksti-elementti
 li1Elem.appendChild(text1Elem);
-// TODO: tee loput tarvittavat elementit
+// toinen li-elementti, Huomaa lyhyempi tapa!
+const li2Elem = document.createElement('li');
+li2Elem.textContent = 'Second item';        // teksti suoraan li-elementille!
 
 // lisätään ul-elementin sisään luodut li-elementit
 ulElem.appendChild(li1Elem);
+ulElem.appendChild(li2Elem);
 
 // TODO: lisää css-määritys
 
